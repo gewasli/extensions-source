edc6ca771d32f3cd840f6445668caf0527ebfd93
gewasli <100284315+gewasli@users.noreply.github.com>
2026-08-29 12:05:18 +0800

    Update build.gradle.kts

--- src/zh/itsavg/build.gradle.kts
+++ src/zh/itsavg/build.gradle.kts
@@ -7,17 +7,11 @@ plugins {
 keiyoushi {
     name = "ITSACG"
     versionCode = 1
-    versionName = "1.0"
     contentWarning = ContentWarning.NSFW
-    // libVersion 不要手动写，由 catalog alias 接管，删除该行
 
     source {
         name = "福利漫畫"
         lang = "zh"
         baseUrl = "https://www.itsacgaa.online/"
-        // 示例：JS引擎，需要同目录新建 source.js 写爬虫逻辑
-        engine {
-            js(file("source.js"))
-        }
     }
 }
